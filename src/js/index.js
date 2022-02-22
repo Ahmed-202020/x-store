881b4a6699fbfa29f77cca46c353ead34bd887df
Ahmed-202020 <ak812554@gmail.com>
2022-02-22 16:32:01 +0200

    finished design index.html

--- src/js/index.js
+++ src/js/index.js
@@ -8,4 +8,25 @@ $(function(){
         $("html").attr("dir" , "rtl") ;
     }) ; 
 
+    $('.owl-carousel').owlCarousel({
+        // loop: true,
+        rtl:true,
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        autoHeight : true,
+        margin: 15,
+        nav: true,
+        
+        responsive: {
+            0: {
+                items: 1 , 
+            },
+            600: {
+                items: 2 , 
+            },
+            1000: {
+                items: 6 , 
+            },
+        }
+    }) ;
 })
